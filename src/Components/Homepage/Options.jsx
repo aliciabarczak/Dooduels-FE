2c40c82e13a830088c815448855c2c5ad214cd82
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-27 14:02:37 +0100

    add basic functionality to Options

--- src/Components/Homepage/Options.jsx
+++ src/Components/Homepage/Options.jsx
@@ -1,5 +1,15 @@
+import "./../Options.css";
 const Options = () => {
-
+  return (
+    <div className="Homepage-Options">
+      <p>Add Room</p>
+      <form>
+        <label>
+          <input type="text" placeholder="Search room..." />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default Options;
