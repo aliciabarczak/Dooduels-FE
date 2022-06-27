96852bf8234379ddeaaf9a1722aabe8947a1eb4c
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-27 12:29:50 +0100

    add all empty components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header.jsx";
+import BriefIns from "./Components/BriefIns.jsx";
+import Buttons from "./Components/Buttons.jsx";
+import RoomList from "./Components/RoomList.jsx";
+import Leaderboard from "./Components/Leaderboard.jsx";
+import LoginPage from "./Components/LoginPage.jsx";
+import Roompage from "./Components/Roompage.jsx";
+import UserPage from "./Components/UserPage.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <div className="Homepage">
+          <BriefIns />
+          <Buttons />
+          <RoomList.jsx />
+          <Leaderboard />
+        </div>
+        <LoginPage />
+        <Roompage />
+        <UserPage />
+      </main>
     </div>
   );
 }
