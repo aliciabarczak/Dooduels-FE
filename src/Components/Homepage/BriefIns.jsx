e826934a10c12a042481b6e017caff1c725a7302
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-28 09:33:22 +0100

    change font size

--- src/Components/Homepage/BriefIns.jsx
+++ src/Components/Homepage/BriefIns.jsx
@@ -1,9 +1,8 @@
 const BriefIns = () => {
   return (
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam
+    <p className="BriefIns">
+      Brief instructions, brief instructions, brief instructions, brief
+      instructions.
     </p>
   );
 };
