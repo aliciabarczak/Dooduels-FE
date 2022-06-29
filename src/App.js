43597525adbdec6cf0bc901079de3ecc3663d2b0
Javier <javiergarna.2107@gmail.com>
2022-06-29 10:38:47 +0100

    fix merge problems LoginPopUpBox

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
         <Header />
         <main>
           <RoomPage />
-          <UserPage />
           <Routes>
             <Route
               path="/"
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({}) => {
   const location = useLocation();
   const { loggedUser } = useContext(userContext);
 
-    if (location.pathname === "/login" || location.pathname === `/users/${loggedUser.user_id}`) {
+    if (location.pathname === `/users/${loggedUser.user_id}`) {
         return <header className="header">
             <h1>Dooduels</h1>
         </header>
@@ -22,14 +22,28 @@ const Header = ({}) => {
     return (
       <header className="header">
         <h1>Dooduels</h1>
-        <Link to={`/users/${loggedUser.user_id}`}>
-            <div className="home-user">
-                <img className="home-profile-pic" src={loggedUser.profile_pic} alt="example"/>
-                <p className="home-username">{loggedUser.username}</p>
-            </div>
+        <Link to={`/login`} key="login" className="login-button">
+          Login
         </Link>
+      </header>
+    );
+  }
+
+  return (
+    <header className="header">
+      <h1>Dooduels</h1>
+        <Link to={`/users/${loggedUser.user_id}`}>
+          <div className="home-user">
+          <img
+            className="home-profile-pic"
+            src={loggedUser.profile_pic}
+            alt="example"
+          />
+          <p className="home-username">{loggedUser.username}</p>
+        </div>
+      </Link>
     </header>
   );
 };
 
-export default Header
+export default Header;
