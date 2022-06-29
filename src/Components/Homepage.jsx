81cb38532b55a8bfce4174254de40b64299dc24f
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-29 09:03:13 +0100

    add key info component

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -3,12 +3,14 @@ import Options from "./Homepage/Options";
 import RoomList from "./Homepage/RoomList";
 import LeaderBoard from "./Homepage/Leaderboard";
 import { useState } from "react";
+import KeyInfo from "./Homepage/KeyInfo";
 export default function ({ showPopUp, setShowPopUp }) {
   const [showLogIn, setShowLogIn] = useState(false);
   return (
     <div className="Homepage">
       <BriefIns />
       <Options showPopUp={showPopUp} setShowPopUp={setShowPopUp} />
+      <KeyInfo />
       <RoomList showLogIn={showLogIn} setShowLogIn={setShowLogIn} />
       <LeaderBoard />
     </div>
---
+++ src/Components/Homepage/KeyInfo.jsx
@@ -0,0 +1,8 @@
+export default function KeyInfo() {
+  return (
+    <div className="KeyInfo">
+      <p> Players Online: 5</p>
+      <p> Rooms Open: 13</p>
+    </div>
+  );
+}
