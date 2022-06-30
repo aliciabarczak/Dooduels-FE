b34b6a3947d124972a9d8eb19f58e31a0e65ec07
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-30 11:03:27 +0100

    add addRoom functionality

--- src/Components/Homepage/AddRoomBox.jsx
+++ src/Components/Homepage/AddRoomBox.jsx
@@ -1,13 +1,31 @@
 import "./../../Styling/Homepage.css";
 import { AiOutlineClose } from "react-icons/ai";
+import { useContext, useState } from "react";
+import userContext from "../../contexts/userContext";
+import { addRoom } from "./../../db/utils.js";
+
 export default function AddRoomBox({ showPopUp, setShowPopUp }) {
+  const { loggedUser } = useContext(userContext);
+  const [newRoomName, setNewRoomName] = useState("");
+  const [newRoomMode, setNewRoomMode] = useState("Easy");
+
+  const handleSubmit = () => {
+    addRoom(loggedUser, newRoomName, newRoomMode);
+  };
+  console.log(loggedUser, newRoomName, newRoomMode);
+
   return (
     <div className="AddRoomBox">
       <h2>Add Room</h2>
-      <form className="dropdown">
+      <form className="dropdown" onSubmit={handleSubmit}>
         <label>Select Mode:</label>
-        <select>
-          <option value="BMW">Easy</option>
+        <select
+          className="dropDownMenu"
+          value={newRoomMode}
+          onChange={(event) => {
+            setNewRoomMode(event.target.value);
+          }}>
+          <option value="Easy">Easy</option>
           <option value="Medium"> Medium</option>
           <option value="Hard">Hard</option>
         </select>
@@ -17,6 +35,10 @@ export default function AddRoomBox({ showPopUp, setShowPopUp }) {
             type="text"
             className="roomNameInput"
             placeholder="Room Name"
+            value={newRoomName}
+            onChange={(event) => {
+              setNewRoomName(event.target.value);
+            }}
           />
         </div>
         <AiOutlineClose
