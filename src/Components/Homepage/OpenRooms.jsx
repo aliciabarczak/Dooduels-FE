86d7c87e670cecfc492468aca82df2b9331c270e
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-30 11:03:42 +0100

    add deleteRoom functionality

--- src/Components/Homepage/OpenRooms.jsx
+++ src/Components/Homepage/OpenRooms.jsx
@@ -1,7 +1,15 @@
 import { BsPeopleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import userContext from "../../contexts/userContext";
+import { deleteRoom } from "../../db/utils.js";
 
 export default function OpenRooms({ rooms }) {
+  const { loggedUser } = useContext(userContext);
+  const handleDelete = (room_id) => {
+    deleteRoom(room_id);
+  };
+
   return (
     <>
       <h2>Open Rooms</h2>
@@ -9,7 +17,7 @@ export default function OpenRooms({ rooms }) {
         {rooms.map((room, index) => {
           if (!room.full)
             return (
-              <div className="Room" key={room.room_id}>
+              <section className="Room" key={room.room_id}>
                 <div>
                   <li>{room.room_name}</li>
                   <p className="mode">{room.mode}</p>
@@ -23,13 +31,23 @@ export default function OpenRooms({ rooms }) {
                     enter
                   </Link>
                 </button>
-                <p>
-                  <BsPeopleFill className="peopleIcon" />
-                </p>
-                <p>
-                  {room.players.length ? `${room.players.length}/5` : "0/5"}
-                </p>
-              </div>
+                {loggedUser.user_name === room.host.user_name ? (
+                  <button
+                    className="button"
+                    onClick={() => handleDelete(room.room_id)}>
+                    delete
+                  </button>
+                ) : (
+                  <div>
+                    <p>
+                      <BsPeopleFill className="peopleIcon" />
+                    </p>
+                    <p>
+                      {room.players.length ? `${room.players.length}/5` : "0/5"}
+                    </p>
+                  </div>
+                )}
+              </section>
             );
         })}
       </ul>
