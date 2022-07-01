27a96b624b57763edd7ef4f5e7a30d14e1b8c0f0
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-07-01 10:54:39 +0100

    adds basic playerboard functionality

--- src/Components/Roompage.jsx
+++ src/Components/Roompage.jsx
@@ -3,81 +3,35 @@ import { useContext, useState, useEffect } from "react";
 import userContext from "../contexts/userContext.js";
 import LogInPopUpBox from "./Homepage/LogInPopUpBox";
 import { useLocation, Link } from "react-router-dom";
-import { getRoomById, getUserbuUsername } from "../db/utils";
+import { getRoomById, getUserbyUsername, addPlayerToRoom } from "../db/utils";
 import Playerboard from "./Roompage/Playerboard";
 
 const Roompage = () => {
   const { loggedUser } = useContext(userContext);
   const location = useLocation();
   const roomID = location.pathname.split("/")[2];
-
   const [roompageRoom, setRoompageRoom] = useState({});
-  // const [players, setPlayers] = useState([]);
-
   useEffect(() => {
     getRoomById(roomID, setRoompageRoom);
-  }, []);
-
-  // roompageRoom.players.map((player) => {
-  //   getUserKeyByUsername(
-  //     player,
-  //     setPlayers((currPlayers) => {
-  //       return [player, ...currPlayers];
-  //     })
-  //   );
-  // });
-
-  const exampleResponse = {
-    full: false,
-    host: "tomtickle",
-    messages: {
-      "-N5iVlC-p5xgUItrAMGV": "Hi mum, i'm on TV",
-      "-N5iVo0GHu537d7YNEDg": "hello world",
-      "-N5iVsWCXjTPWzay13ha": "howdy",
-      "-N5iVzxwc9rGrq0LZHNf": "bananas in pjamas",
-    },
-    mode: "'easy'",
-    players: ["jessjelly", "sparkles", "sparkles"],
-    room_name: "Room 1",
-    room_id: "1",
-  };
+    addPlayerToRoom(loggedUser, roomID);
+  }, [roomID]);
 
   return (
     <section>
+      {!loggedUser ? <LogInPopUpBox /> : null}
       <div className="Roompage">
-        <h1 className="roomTitle">{exampleResponse.room_name}</h1>
-        <p className="modeRoompage">mode: {exampleResponse.mode.split("'")}</p>
+        <h1 className="roomTitle">{roompageRoom.room_name}</h1>
+        <p className="modeRoompage">mode: {roompageRoom.mode}</p>
         <div className="RoomPageButtons">
-          {loggedUser === exampleResponse.host ? (
-            <button
-              className="ready-button"
-              onClick={() => {
-                console.log(
-                  "should take to the game page and start the game as the person drawing"
-                );
-              }}>
-              Start Game!
-            </button>
-          ) : (
-            <button
-              className="ready-button"
-              onClick={() => {
-                console.log(
-                  "should take to the game page and make them game participants"
-                );
-              }}>
-              Ready!
-            </button>
-          )}
+          <button className="ready-button">Ready!</button>
           <button className="exit-button">
             <Link to="/"> Exit</Link>
           </button>
         </div>
         <h2>Players</h2>
-        <Playerboard />
+        <Playerboard roompageRoom={roompageRoom} />
         <h2>Chat</h2>
         <div className="chat"></div>
-        {!loggedUser ? <LogInPopUpBox /> : null}
       </div>
     </section>
   );
--- src/Components/Roompage/Playerboard.jsx
+++ src/Components/Roompage/Playerboard.jsx
@@ -1,59 +1,28 @@
 import "./../../Styling/Roompage.css";
+import { useContext, useState, useEffect } from "react";
+import userContext from "../../contexts/userContext.js";
+import { addPlayerToRoom } from "../../db/utils";
 
-export default function Playerboard() {
-  const examplePlayers = [
-    {
-      avatar_url:
-        "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
-      description: "A cool lass",
-      friends: [[Object], [Object], [Object]],
-      password: "password123",
-      points: 450,
-      status: "offline",
-      user_name: "jessjelly",
-      user_id: "-N5_Z_EYyZBrivsr1pdL",
-    },
-    {
-      avatar_url:
-        "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
-      description: "A cool lass",
-      friends: [[Object], [Object], [Object]],
-      password: "password123",
-      points: 350,
-      status: "offline",
-      user_name: "jessy",
-      user_id: "-N5_Z_EYyZBrivsr1pdL",
-    },
-    {
-      avatar_url:
-        "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
-      description: "A cool lass",
-      friends: [[Object], [Object], [Object]],
-      password: "password123",
-      points: 50,
-      status: "offline",
-      user_name: "jellyjessy",
-      user_id: "-N5_Z_EYyZBrivsr1pdL",
-    },
-  ];
+export default function Playerboard({ roompageRoom }) {
+  const { loggedUser } = useContext(userContext);
+  const [players, setPlayers] = useState([]);
 
-  // const players = [];
-  //   exampleResponse.players.map((username) => {
-  //     getUserByUsername(username, setPlayer)
-  //     players.push(player)
-  // })}
+  console.log(roompageRoom);
+
   return (
     <>
       <div className="playerboard">
-        {examplePlayers.map((player) => {
-          return (
-            <div className="playerInfo" key={player.user_id}>
-              <img src={player.avatar_url} className="profile-pic" />
-              <b>{player.user_name}</b>
-              <p>points: {player.points}</p>
-            </div>
-          );
-        })}
+        {loggedUser
+          ? roompageRoom.players.map((player, index) => {
+              return (
+                <div className="playerInfo" key={index}>
+                  {/* <img src={player.avatar_url} className="profile-pic" /> */}
+                  <b>{player}</b>
+                  {/* <p>points: {player.points}</p> */}
+                </div>
+              );
+            })
+          : null}
       </div>
     </>
   );
