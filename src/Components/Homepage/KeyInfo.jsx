97e7258e4caf2ebc6582a948357408a65f9f3b64
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-07-01 17:09:13 +0100

    add live data to key info

--- src/Components/Homepage/KeyInfo.jsx
+++ src/Components/Homepage/KeyInfo.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import { getAllUsers, getAllRooms } from "../../db/utils";
+
 export default function KeyInfo() {
+  const [allUsers, setAllUsers] = useState([]);
+  const [allRooms, setAllRooms] = useState([]);
+  useEffect(() => {
+    getAllUsers(setAllUsers);
+    getAllRooms(setAllRooms);
+  }, []);
+
+  let onlineCount = 0;
+  allUsers.map((user) => {
+    if (user.status === "Online") onlineCount++;
+  });
+
   return (
     <div className="KeyInfo">
-      <p> Players Online: 5</p>
-      <p> Rooms Open: 13</p>
+      <p> Players Online: {onlineCount}</p>
+      <p> Rooms Open: {allRooms.length}</p>
     </div>
   );
 }
