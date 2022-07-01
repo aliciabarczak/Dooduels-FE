22c56f70e63a44a5bdf96cc587368f2eecb30387
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-07-01 16:43:17 +0100

    complete delete room functionality

--- src/Components/Homepage/OpenRooms.jsx
+++ src/Components/Homepage/OpenRooms.jsx
@@ -1,10 +1,16 @@
 import { BsPeopleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import userContext from "../../contexts/userContext.js";
+import { deleteRoom } from "../../db/utils.js";
 
 export default function OpenRooms({ rooms }) {
   const { loggedUser } = useContext(userContext);
+  const [cannotDelete, setCannotDelete] = useState(false);
+  const sendMsg = () => {
+    return <p>Cannot delete room</p>;
+  };
+
   return (
     <>
       <h2>Open Rooms</h2>
@@ -12,27 +18,38 @@ export default function OpenRooms({ rooms }) {
         {rooms.map((room, index) => {
           if (!room.full)
             return (
-              <section className="Room" key={index}>
-                <div>
+              <section
+                key={index}
+                className={index % 2 === 1 ? "Room dark" : "Room"}>
+                <div className="roomInfo">
                   <li>{room.room_name}</li>
                   <p className="mode">{room.mode}</p>
                 </div>
-
-                {loggedUser.user_id === room.host.user_id ? (
-                  <select className="button dropDownBtn">
-                    <option value="Enter">enter</option>
-                    <option value="Delete"> delete</option>
-                  </select>
-                ) : (
+                <div className="buttons">
                   <button className="button">
                     <Link
                       to={`/rooms/${room.room_id}`}
                       key="login"
                       className="login-button">
                       enter
-                    </Link>{" "}
+                    </Link>
                   </button>
-                )}
+                  {loggedUser.user_id === room.host.user_id ? (
+                    <button
+                      className={
+                        room.players.length === 0 ? "button" : "disabledButton"
+                      }
+                      onClick={
+                        room.players.length === 0
+                          ? () => deleteRoom(room.room_id)
+                          : null
+                      }>
+                      delete
+                    </button>
+                  ) : (
+                    <p>Host: {room.host.user_name}</p>
+                  )}
+                </div>
                 <div className="peopleInfo">
                   <p>
                     <BsPeopleFill className="peopleIcon" />
