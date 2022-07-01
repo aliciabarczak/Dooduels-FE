ca43739ca6dac43bf32f591ba6536ef15b1221b6
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-07-01 13:45:09 +0100

    add delete room option to the host

--- src/Components/Homepage/OpenRooms.jsx
+++ src/Components/Homepage/OpenRooms.jsx
@@ -1,7 +1,10 @@
 import { BsPeopleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import userContext from "../../contexts/userContext.js";
 
 export default function OpenRooms({ rooms }) {
+  const { loggedUser } = useContext(userContext);
   return (
     <>
       <h2>Open Rooms</h2>
@@ -15,20 +18,29 @@ export default function OpenRooms({ rooms }) {
                   <p className="mode">{room.mode}</p>
                 </div>
 
-                <button className="button">
-                  <Link
-                    to={`/rooms/${room.room_id}`}
-                    key="login"
-                    className="login-button">
-                    enter
-                  </Link>
-                </button>
-                <p>
-                  <BsPeopleFill className="peopleIcon" />
-                </p>
-                <p>
-                  {room.players.length ? `${room.players.length}/5` : "0/5"}
-                </p>
+                {loggedUser.user_id === room.host.user_id ? (
+                  <select className="button dropDownBtn">
+                    <option value="Enter">enter</option>
+                    <option value="Delete"> delete</option>
+                  </select>
+                ) : (
+                  <button className="button">
+                    <Link
+                      to={`/rooms/${room.room_id}`}
+                      key="login"
+                      className="login-button">
+                      enter
+                    </Link>{" "}
+                  </button>
+                )}
+                <div className="peopleInfo">
+                  <p>
+                    <BsPeopleFill className="peopleIcon" />
+                  </p>
+                  <p>
+                    {room.players.length ? `${room.players.length}/5` : "0/5"}
+                  </p>
+                </div>
               </section>
             );
         })}
