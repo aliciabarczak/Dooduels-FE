68ee3bfbdcdad258edcfb87aac2609c366ff2ccb
philipwalton123 <philipwalton123@googlemail.com>
2022-07-01 17:47:54 +0100

    make a small fix

--- src/Components/Roompage.jsx
+++ src/Components/Roompage.jsx
@@ -3,7 +3,7 @@ import { useContext, useState, useEffect } from "react";
 import userContext from "../contexts/userContext.js";
 import LogInPopUpBox from "./Homepage/LogInPopUpBox";
 import { useLocation, Link } from "react-router-dom";
-import { getRoomById, addPlayerToRoom } from "../db/utils";
+import { getRoomById, addPlayerToRoom } from "../db/utils.js";
 import Playerboard from "./Roompage/Playerboard";
 import Chat from "./Chatrooms/Chat";
 import { goOffline } from "firebase/database";
@@ -15,9 +15,12 @@ const Roompage = () => {
   const roomID = location.pathname.split("/")[2];
   const [roompageRoom, setRoompageRoom] = useState({});
   useEffect(() => {
+    console.log(">>>>", roomID)
     getRoomById(roomID, setRoompageRoom);
-    addPlayerToRoom(loggedUser, roomID);
-  }, [roomID]);
+    if (loggedUser) {
+      addPlayerToRoom(loggedUser, roomID);
+    }
+  }, []);
 
   console.log(roompageRoom);
 
@@ -36,7 +39,10 @@ const Roompage = () => {
           </button>
         </div>
         <h2>Players</h2>
-        <Playerboard roompageRoom={roompageRoom} />
+        {
+          Object.keys(roompageRoom).length ? <Playerboard roompageRoom={roompageRoom} /> : null
+        }
+        
         <h2>Chat</h2>
         <div className="chat"></div>
       </div>
