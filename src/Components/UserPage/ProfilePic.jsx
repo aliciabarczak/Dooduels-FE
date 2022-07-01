ed9dae96c82455723d8b1abcd5a2b9fe7b9aac99
Javier <javiergarna.2107@gmail.com>
2022-07-01 12:32:05 +0100

    fix updateUserImage conflicts

--- src/Components/UserPage/ProfilePic.jsx
+++ src/Components/UserPage/ProfilePic.jsx
@@ -1,21 +1,31 @@
 import * as filestack from 'filestack-js';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import userContext from '../../contexts/userContext';
-import { updateUserImage } from '../../db/utils';
+import { getUserById, updateUserImage } from '../../db/utils';
 
 const ProfilePic = () => {
-    const { loggedUser } = useContext(userContext);
-    const client = filestack.init("An1BmavtPQxOZ9tAQhqAPz");
-    const options = {
-        onFileUploadFinished(file) {
-            updateUserImage(loggedUser.user_id, file.url);
-        }
-    };
-    const picker = client.picker(options);
+    const { loggedUser, setLoggedUser, users } = useContext(userContext);
+    const [ currUser, setCurrUser ] = useState({})
+    // const client = filestack.init("An1BmavtPQxOZ9tAQhqAPz");
+    // const options = {
+    //     onFileUploadFinished(file) {
+    //         updateUserImage(loggedUser.user_id, file.url);
+    //     },
+    //     onClose() {
+    //         getUserById(loggedUser.user_id, setCurrUser);
+    //         setLoggedUser(currUser);
+    //         window.localStorage.setItem("loggedUser", JSON.stringify(currUser))
+    //     }
+    // };
+    
+    // const picker = client.picker(options)
 
-    picker.open();
 
+    // useEffect(() => {
+    //     picker.open();
+    // })
+    
     return (
         <div>
             <Link to={`/`} className="back-button">Back to Home</Link>
