2a25f25eac18f8dffe340e26a9b5d5cbbabf9dd7
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-07-04 17:55:50 +0100

    add basic timer functionality

--- src/Components/Gamepage/Gamepage.js
+++ src/Components/Gamepage/Gamepage.js
@@ -8,18 +8,56 @@ export default function Gamepage() {
   const { room_id } = useParams();
   const [room, setRoom] = useState();
 
+  function formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
+  }
+
+  const TIME_LIMIT = 60;
+  let timePassed = 0;
+  let timeLeft = TIME_LIMIT;
+
+  let timerInterval = null;
+  function startTimer() {
+    timerInterval = setInterval(() => {
+      if (timeLeft === 0) {
+        document.getElementById("timer").innerHTML = "TIME IS UP";
+        timePassed = 60;
+        return;
+      }
+      timePassed = timePassed += 1;
+      timeLeft = TIME_LIMIT - timePassed;
+      document.getElementById("timer").innerHTML = formatTime(timeLeft);
+    }, 1000);
+  }
+
   useEffect(() => {
     getRoomById(room_id, setRoom);
-    console.log("hello");
+    startTimer();
   }, []);
 
-  console.log(room, "<<<room in gamepage");
   return (
-    <>
+    <div className="gamepage">
+      <div class="base-timer">
+        <svg
+          class="base-timer__svg"
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg">
+          <g class="base-timer__circle">
+            <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
+          </g>
+        </svg>
+        <span id="timer">{formatTime(timeLeft)}</span>
+      </div>
       <h1>This will be a game page</h1>
+
       {typeof room === "object" ? (
         <Canvas room_id={room_id} room={room} />
       ) : null}
-    </>
+    </div>
   );
 }
