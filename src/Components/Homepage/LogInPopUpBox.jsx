c80e07c95926f602fb5f6a8c1c8a2ce31f5a6bd1
Javier <javiergarna.2107@gmail.com>
2022-07-04 15:41:08 +0100

    add sign up

--- src/Components/Homepage/LogInPopUpBox.jsx
+++ src/Components/Homepage/LogInPopUpBox.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import userContext from "../../contexts/userContext.js";
-import { updateUserStatus } from "../../db/utils.js";
+import { addUser, updateUserStatus } from "../../db/utils.js";
 import "./../../Styling/Roompage.css";
 
 export default function LogInPopUpBox() {
@@ -8,17 +8,34 @@ export default function LogInPopUpBox() {
   const { users } = useContext(userContext);
   const [input, setInput] = useState("");
   const [message, setMessage] = useState(false);
+  const [message2, setMessage2] = useState(true);
+  const [message3, setMessage3] = useState(false);
+  const [message4, setMessage4] = useState(false);
+  const [signUp, setSignUp ] = useState(false);
+  const regex = /\W/g
 
   useEffect(() => {
     const data = window.localStorage.getItem("loggedUser");
     setLoggedUser(JSON.parse(data));
   }, []);
 
+  const handleSignUp = () => {
+    setMessage3(false);
+    setMessage2(false);
+    setSignUp(true);
+  };
+
+  const handleBack = () => {
+    setMessage(false);
+    setMessage4(false);
+    setSignUp(false);
+  };
+
   const handleChange = (event) => {
     setInput(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleLogInSubmit = (event) => {
     event.preventDefault();
     users.map((user) => {
       if (user.user_name === input) {
@@ -32,25 +49,86 @@ export default function LogInPopUpBox() {
     })
   };
 
+  const handleSignUpSubmit = (event) => {
+    event.preventDefault();
+    setMessage2(false);
+    users.map((user) => {
+      if (user.user_name === input) {
+        setMessage3(false);
+        setMessage2(true);
+      }
+    });
+    if(!message2 && input.length !== 0 && !regex.test(input)) {
+      addUser({
+        user_name: input,
+        avatar_url: "https://i.pinimg.com/originals/99/a8/3e/99a83e22b4c160d36e1697b4139c803f.jpg",
+        password: "password123",
+        points: 0
+      });
+      setMessage3(false);
+      setMessage4(true);
+      setSignUp(false);
+    } 
+    if (regex.test(input)) {
+      console.log("Ehllo")
+      setMessage3(true);
+    };
+  };
+  
   return (
     <div className="LogInBox">
-      <h2>Please log in</h2>
-      <div>
-        <form onSubmit={handleSubmit} className="form">
-          <label className="login-label">Enter your username:</label>
-          <input
-            className="username-input"
-            type="text"
-            onChange={handleChange}
-          />
-          {message ? (
-            <p className="error-message">We couldn't find a user with that name. Please try again.</p>
-          ) : null}
-          <button type="submit" className="login-submit-button">
-            Submit
-          </button>
-        </form>
-      </div>
+      {signUp ? (
+        <div>
+          <h2>Please sign up</h2>
+          <form onSubmit={handleSignUpSubmit} className="form">
+            <label className="login-label">Enter your username:</label>
+            <input
+              className="username-input"
+              type="text"
+              onChange={handleChange}
+            />
+            {message2 ? (
+              <p className="error-message">This username is already in use.</p>
+            ) : null}
+            {message3 ? (
+              <p className="error-message">Please, type a valid username.</p>
+            ) : null}
+            <div className="login-buttons">
+            <button type="button" onClick={handleBack} className="login-submit-button">
+              Back 
+              </button>
+              <button type="submit" className="login-submit-button">
+              Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      ) : 
+        <div>
+          <h2>Please log in</h2>
+          <form onSubmit={handleLogInSubmit} className="form">
+            <label className="login-label">Enter your username:</label>
+            <input
+              className="username-input"
+              type="text"
+              onChange={handleChange}
+            />
+            {message ? (
+              <p className="error-message">We couldn't find a user with that name. Please try again.</p>
+            ) : null}
+            {message4 ? (
+              <p className="error-message">You created a new user succesfully. Please, log in.</p>
+            ) : null}
+            <div className="login-buttons">
+            <button type="button" onClick={handleSignUp} className="login-submit-button">
+              Sign Up 
+              </button>
+              <button type="submit" className="login-submit-button">
+              Submit
+              </button>
+            </div>
+          </form>
+        </div>}
     </div>
   );
 }
