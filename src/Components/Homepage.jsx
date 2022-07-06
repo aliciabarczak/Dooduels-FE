7ddcf715e9532d492f0232f34314811dbf00ebb8
Javier <javiergarna.2107@gmail.com>
2022-07-06 16:52:02 +0100

    add playerlist updating in real-time

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -10,14 +10,10 @@ import { removePlayerFromRoom } from "../db/utils";
 export default function Homepage({ showPopUp, setShowPopUp }) {
   const { loggedUser, lastRoomId, setLastRoomId } = useContext(userContext);
   const [showLogIn, setShowLogIn] = useState(false);
-
-  useEffect(() => {
-    const data = window.localStorage.getItem("lastRoomId");
-    setLastRoomId(data);
-    if (loggedUser) {
-      removePlayerFromRoom(loggedUser.user_id, lastRoomId);
-    }
-  }, []);
+  const data = window.localStorage.getItem("lastRoomId");
+  if (data) {
+      removePlayerFromRoom(loggedUser.user_id, data);
+  }
 
   return (
     <div className="Homepage">
